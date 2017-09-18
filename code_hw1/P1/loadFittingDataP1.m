f68e90302f8aeb383868fcb791611f3888e60284
Rebecca Carlson <becca@dhcp-18-111-31-84.dyn.MIT.EDU>
2017-09-18 10:04:27 -0400

    Hw1 first commit

---
+++ code_hw1/P1/loadFittingDataP1.m
@@ -0,0 +1,6 @@
+function [X, y] = loadFittingDataP1()
+
+% return the data matrix X (100 by 10) and label vector y (100 by 1)
+
+X = importdata('/Users/becca/Documents/Google Drive/Dropbox/Grad School/ML/code_hw1/P1/fittingdatap1_x.txt');
+y = importdata('/Users/becca/Documents/Google Drive/Dropbox/Grad School/ML/code_hw1/P1/fittingdatap1_y.txt');
